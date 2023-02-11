2232b4cda10668187f92662d3983229f7f40293d
rnewkirk <ryannewkirk2024@u.northwestern.edu>
2023-02-11 16:54:35 -0600

    the pizza is here (tailwindcss formatting)

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -59,59 +59,63 @@ export default function Home() {
   };
 
   return (
-    <div className="flex items-center mx-auto min-h-screen justify-center bg-purple-500">
+    <div className="flex items-center mx-auto min-h-screen justify-center bg-green-500">
       <main className="w-full gap-4 flex flex-col items-center justify-center h-full">
-        <div className="flex items-center mx-auto min-h-screen justify-center bg-purple-500">
-          <div className="w-1/2 h-full">
-            <p className="w-1/2 font-bold text-white text-xl">Profile Info</p>
-          </div>
-          <div className="w-1/2 h-full">
-            <>
-              <p className="font-bold text-white text-xl">
-                Your name: {myProfile.name}
-              </p>
-              <div className="flex flex-col justify-end bg-white h-[20rem] min-w-[33%] rounded-md shadow-md ">
-                <div className="h-full last:border-b-0 overflow-y-scroll">
-                  {messages.map((msg, i) => {
-                    return (
-                      <div
-                        className="w-full py-1 px-2 border-b border-gray-200"
-                        key={i}
+          <div className="w-full flex items-center mx-auto min-h-screen justify-center">
+            
+            <div className="w-1/2 h-screen bg-gray-500 p-2 hover:bg-gradient-to-r">
+              <p className="w-1/2 h-full font-bold text-white text-xl">Profile Info</p>
+            </div>
+
+            <div className="w-1/2 h-screen bg-purple-500">
+              <>
+                <p className="font-bold text-white text-xl">
+                  Your name: {myProfile.name}
+                </p>
+                <div className="flex flex-col justify-end bg-white h-[20rem] min-w-[33%] rounded-md shadow-md ">
+                  <div className="h-full last:border-b-0 overflow-y-scroll">
+                    {messages.map((msg, i) => {
+                      return (
+                        <div
+                          className="w-full py-1 px-2 border-b border-gray-200"
+                          key={i}
+                        >
+                          {msg.authorName} : {msg.message}
+                        </div>
+                      );
+                    })}
+                  </div>
+                  <div className="border-t border-gray-300 w-full flex rounded-bl-md">
+                    <input
+                      type="text"
+                      placeholder="New message..."
+                      value={message}
+                      className="outline-none py-2 px-2 rounded-bl-md flex-1"
+                      onChange={(e) => setMessage(e.target.value)}
+                      onKeyUp={handleKeypress}
+                    />
+                    <div className="border-l border-gray-300 flex justify-center items-center  rounded-br-md group hover:bg-purple-500 transition-all">
+                      <button
+                        className="group-hover:text-white px-3 h-full"
+                        onClick={() => {
+                          sendMessage();
+                        }}
                       >
-                        {msg.authorName} : {msg.message}
-                      </div>
-                    );
-                  })}
-                </div>
-                <div className="border-t border-gray-300 w-full flex rounded-bl-md">
-                  <input
-                    type="text"
-                    placeholder="New message..."
-                    value={message}
-                    className="outline-none py-2 px-2 rounded-bl-md flex-1"
-                    onChange={(e) => setMessage(e.target.value)}
-                    onKeyUp={handleKeypress}
-                  />
-                  <div className="border-l border-gray-300 flex justify-center items-center  rounded-br-md group hover:bg-purple-500 transition-all">
-                    <button
-                      className="group-hover:text-white px-3 h-full"
-                      onClick={() => {
-                        sendMessage();
-                      }}
-                    >
-                      Send
-                    </button>
+                        Send
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </>
+              </>
+            </div>
+
+            <div className="w-1/2 h-screen 
+                              bg-cover bg-center bg-[url('https://i.ibb.co/L5xXrzj/istockphoto-486407276-612x612-transformed-1.jpg')]
+                              justify-center">
+                <p className="font-bold text-center text-black text-xl">TAKE IT TO THE NEXT LEVEL</p>
+                
+            </div>
           </div>
-          <div className="w-1/2 h-full">
-            <p className="w-1/2 font-bold text-white text-xl">
-              TAKE IT TO THE NEXT LEVEL
-            </p>
-          </div>
-        </div>
       </main>
     </div>
   );
