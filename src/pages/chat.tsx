ad60df94de39a05152455621797e3afc6a53bc45
Andrew Pulver <64996411+Pulverizer2020@users.noreply.github.com>
2023-02-11 18:50:08 -0600

    some security

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,6 +1,7 @@
 import io from "socket.io-client";
 import { useState, useEffect } from "react";
 import { useMyProfile } from "../utils/context/myProfileContext";
+import { useRouter } from "next/router";
 
 let socket;
 
@@ -19,11 +20,13 @@ enum ActionType {
   Popup,
   Sound,
   Dox,
-  GiftSwap
-};
+  GiftSwap,
+}
 
 export default function Home() {
   const myProfile = useMyProfile();
+
+  const router = useRouter();
 
   const [level, setLevel] = useState(0);
   const [partnerRequest, setPartnerRequest] = useState(false);
@@ -33,6 +36,12 @@ export default function Home() {
   useEffect(() => {
     socketInitializer();
   }, []);
+
+  useEffect(() => {
+    if (!myProfile.id || !myProfile.name || !myProfile.email) {
+      router.push("/");
+    }
+  }, [myProfile]);
 
   const socketInitializer = async () => {
     // We just call it because we don't need anything else out of it
@@ -57,14 +66,14 @@ export default function Home() {
         case ActionType.Popup: {
           alert("You have been alerted");
           break;
-        } case ActionType.Sound: {
-
+        }
+        case ActionType.Sound: {
           break;
-        } case ActionType.Dox: {
-          
+        }
+        case ActionType.Dox: {
           break;
-        } case ActionType.GiftSwap: {
-          
+        }
+        case ActionType.GiftSwap: {
           break;
         }
       }
@@ -122,79 +131,81 @@ export default function Home() {
   return (
     <div className="flex items-center mx-auto min-h-screen justify-center bg-green-500">
       <main className="w-full gap-4 flex flex-col items-center justify-center h-full">
-          <div className="w-full flex items-center mx-auto min-h-screen justify-center">
-            
-            <div className="w-1/2 h-screen bg-gray-500 p-2 hover:bg-gradient-to-r">
-              <p className="w-1/2 h-full font-bold text-white text-xl">Profile Info</p>
-            </div>
+        <div className="w-full flex items-center mx-auto min-h-screen justify-center">
+          <div className="w-1/2 h-screen bg-gray-500 p-2 hover:bg-gradient-to-r">
+            <p className="w-1/2 h-full font-bold text-white text-xl">
+              Profile Info
+            </p>
+          </div>
 
-            <div className="w-1/2 h-screen bg-purple-500">
-              <>
-                <p className="font-bold text-white text-xl">
-                  Your name: {myProfile.name}
-                </p>
-                <div className="flex flex-col justify-end bg-white h-[20rem] min-w-[33%] rounded-md shadow-md ">
-                  <div className="h-full last:border-b-0 overflow-y-scroll">
-                    {messages.map((msg, i) => {
-                      return (
-                        <div
-                          className="w-full py-1 px-2 border-b border-gray-200"
-                          key={i}
-                        >
-                          {msg.authorName} : {msg.message}
-                        </div>
-                      );
-                    })}
-                  </div>
-                  <div className="border-t border-gray-300 w-full flex rounded-bl-md">
-                    <input
-                      type="text"
-                      placeholder="New message..."
-                      value={message}
-                      className="outline-none py-2 px-2 rounded-bl-md flex-1"
-                      onChange={(e) => setMessage(e.target.value)}
-                      onKeyUp={handleKeypress}
-                    />
-                    <div className="border-l border-gray-300 flex justify-center items-center  rounded-br-md group hover:bg-purple-500 transition-all">
-                      <button
-                        className="group-hover:text-white px-3 h-full"
-                        onClick={() => {
-                          sendMessage();
-                        }}
+          <div className="w-1/2 h-screen bg-purple-500">
+            <>
+              <p className="font-bold text-white text-xl">
+                Your name: {myProfile.name}
+              </p>
+              <div className="flex flex-col justify-end bg-white h-[30rem] min-w-[33%] rounded-md shadow-md ">
+                <div className="h-full last:border-b-0 overflow-y-scroll">
+                  {messages.map((msg, i) => {
+                    return (
+                      <div
+                        className="w-full py-1 px-2 border-b border-gray-200"
+                        key={i}
                       >
-                        Send
-                      </button>
-                    </div>
+                        {msg.authorName} : {msg.message}
+                      </div>
+                    );
+                  })}
+                </div>
+                <div className="border-t border-gray-300 w-full flex rounded-bl-md">
+                  <input
+                    type="text"
+                    placeholder="New message..."
+                    value={message}
+                    className="outline-none py-2 px-2 rounded-bl-md flex-1"
+                    onChange={(e) => setMessage(e.target.value)}
+                    onKeyUp={handleKeypress}
+                  />
+                  <div className="border-l border-gray-300 flex justify-center items-center  rounded-br-md group hover:bg-purple-500 transition-all">
+                    <button
+                      className="group-hover:text-white px-3 h-full"
+                      onClick={() => {
+                        sendMessage();
+                      }}
+                    >
+                      Send
+                    </button>
                   </div>
                 </div>
-                <p className="font-bold text-white text-xl">
-                  Level: {level}
-                </p>
-              </>
-            </div>
-
-            <div className="w-1/2 h-screen 
-                              bg-cover bg-center bg-[url('https://i.ibb.co/L5xXrzj/istockphoto-486407276-612x612-transformed-1.jpg')]
-                              justify-center">
-                {/* Button */}
-                <button className="pushable"
-                onClick={() => {
-                  requestLevelUp();
-                }}>
-                  <span className="front">
-                    TAKE IT TO THE NEXT LEVEL
-                  </span>
-                </button>
-
-                <button onClick={() => {
-                  console.log("sending alert");
-                  sendAction(ActionType.Popup);
-                }}>
-                  Send alert
-                </button>
-                
-            </div>
+              </div>
+              <p className="font-bold text-white text-xl">Level: {level}</p>
+            </>
           </div>
+
+          <div
+            className="w-1/2 h-screen 
+                              bg-cover bg-center bg-[url('https://i.ibb.co/L5xXrzj/istockphoto-486407276-612x612-transformed-1.jpg')]
+                              justify-center"
+          >
+            {/* Button */}
+            <button
+              className="pushable"
+              onClick={() => {
+                requestLevelUp();
+              }}
+            >
+              <span className="front">TAKE IT TO THE NEXT LEVEL</span>
+            </button>
+
+            <button
+              onClick={() => {
+                console.log("sending alert");
+                sendAction(ActionType.Popup);
+              }}
+            >
+              Send alert
+            </button>
+          </div>
+        </div>
       </main>
     </div>
   );
